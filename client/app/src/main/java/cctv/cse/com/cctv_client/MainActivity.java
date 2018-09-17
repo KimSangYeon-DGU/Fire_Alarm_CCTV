8f1f764d37e29e083322be263b37402f21450f01
KimSangYeon-DGU <sy0814k@gmail.com>
2018-09-18 02:27:22 +0900

    Update) Android HD, 119, JSON communication

--- client/app/src/main/java/cctv/cse/com/cctv_client/MainActivity.java
+++ client/app/src/main/java/cctv/cse/com/cctv_client/MainActivity.java
@@ -1,8 +1,10 @@
 package cctv.cse.com.cctv_client;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -21,10 +23,13 @@ import android.widget.TextView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.DataInput;
 import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -34,27 +39,32 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView mIv_frame;
-    ImageView mIv_mark;
-    Bitmap mBtm_receive;
-    NetworkTask networkTask;
-    boolean isConnected;
-    BottomNavigationView bottomNavigationView;
-    List<Item> infoItems;
-    List<Item> logItems;
+    private ImageView mIv_frame;
+    private ImageView mIv_mark;
+    private Bitmap mBtm_receive;
+    private NetworkTask networkTask;
+
+    private BottomNavigationView bottomNavigationView;
+    private List<Item> infoItems;
+    private List<Item> logItems;
     private RecyclerPopupWindow recyclerPopupWindow;
+    private JSONObject inJson;
+    private JSONObject outJson;
+
+    private boolean isConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_land);
-        //String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        //Log.d("REGID",refreshedToken);
-        init();
-        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("REGID",refreshedToken);
+        try {
+            init();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-        Constants.displayWidth = dm.widthPixels;
-        Constants.displayHeight = dm.heightPixels;
 
         mIv_frame = findViewById(R.id.iv_frame);
         mIv_mark = findViewById(R.id.iv_mark);
@@ -67,8 +77,6 @@ public class MainActivity extends AppCompatActivity {
         mIv_frame.setBackgroundColor(Color.WHITE);
         mIv_frame.setVisibility(View.INVISIBLE);
         mIv_mark.setVisibility(View.VISIBLE);
-
-
 
         isConnected  = false;
 
@@ -84,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
                             mIv_frame.setVisibility(View.VISIBLE);
                             // initialize the worker
                             networkTask = new NetworkTask(
-                                    "10.70.22.222",
+                                    "IP address",
                                     8888
                             );
 
@@ -101,20 +109,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.bot_menu_home:
-                        Log.d("DEBUG", "Home Selected");
+                        turnOnAndOffHD("off");
                         break;
 
                     case R.id.bot_menu_hd:
-                        Log.d("DEBUG", "HD Selected");
+                        turnOnAndOffHD("on");
                         break;
 
                     case R.id.bot_menu_list:
-                        Log.d("DEBUG", "List Selected");
+                        turnOnAndOffHD("off");
+
                         if (recyclerPopupWindow == null) {
                             recyclerPopupWindow = new RecyclerPopupWindow(logItems);
                             int windowWidth =(int)(Constants.displayWidth*(1/3.0));
@@ -134,7 +144,8 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.bot_menu_info:
-                        Log.d("DEBUG", "Info Selected");
+                        turnOnAndOffHD("off");
+
                         if (recyclerPopupWindow == null) {
                             recyclerPopupWindow = new RecyclerPopupWindow(infoItems);
                             int windowWidth =(int)(Constants.displayWidth*(1/3.0));
@@ -154,6 +165,12 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.bot_menu_call:
+                        // Turn off TCT socket stream.
+                        turnOnAndOffHD("off");
+
+                        String phone = "119";
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                        startActivity(intent);
                         break;
                 }
                 return true;
@@ -162,7 +179,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void init() {
+    private void turnOnAndOffHD(String status){
+        try {
+            outJson.put("hd", status);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init() throws JSONException {
         infoItems = new ArrayList<>();
         infoItems.add(0, new Item("ID: CAM01", false));
         infoItems.add(1, new Item("Installation date: 09-15-2018", false));
@@ -175,6 +200,16 @@ public class MainActivity extends AppCompatActivity {
         logItems.add(2, new Item("03: CAM01-XX-AA-20xx", false));
         logItems.add(3, new Item("04: CAM01-XX-AA-20xx", false));
         logItems.add(4, new Item("05: CAM01-XX-AA-20xx", false));
+
+        // To get display width and height
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        Constants.displayWidth = dm.widthPixels;
+        Constants.displayHeight = dm.heightPixels;
+
+        outJson = new JSONObject();
+        outJson.put("hd", "off");
+        outJson.put("width", Constants.displayWidth);
+        outJson.put("height", Constants.displayHeight);
     }
 
     public String readUTF8(DataInput in) throws IOException {
@@ -184,17 +219,22 @@ public class MainActivity extends AppCompatActivity {
         return new String(encoded, StandardCharsets.UTF_8);
     }
 
+    public void writeUTF8(String s, DataOutput out) throws IOException {
+        byte [] encoded = s.getBytes(StandardCharsets.UTF_8);
+        out.writeInt(encoded.length);
+        out.write(encoded);
+    }
+
+
     public class NetworkTask extends AsyncTask<Void, Void, Void> {
         String addr;
         int port;
         Socket socket;
-        OutputStream outStream;
+        DataOutputStream out;
         DataInputStream in;
-        JSONObject json;
         String mat_string;
         byte[] raw_data;
-
-        NetworkTask(String addr, int port) {
+        NetworkTask(String addr, int port) throws JSONException {
             this.addr = addr;
             this.port = port;
         }
@@ -214,20 +254,28 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             try{
                 socket = new Socket(this.addr, this.port);
-                outStream = socket.getOutputStream();
+                //outStream = socket.getOutputStream();
+                out = new DataOutputStream(socket.getOutputStream());
                 in = new DataInputStream(socket.getInputStream());
+
+                //writeUTF8(outJson.toString(), out);
+
+                outJson.put("status", "conn");
                 while(true) {
-                    outStream.write("Conn".getBytes("UTF-8"));
-                    json = new JSONObject(readUTF8(in));
+                    writeUTF8(outJson.toString(), out);
+                    //outStream.write("Conn".getBytes("UTF-8"));
+                    inJson = new JSONObject(readUTF8(in));
                     //String leaf_name = json.getString("leaf");
-                    mat_string = json.getString("img");
+                    mat_string = inJson.getString("img");
                     raw_data = Base64.decode(mat_string, Base64.DEFAULT);
                     mBtm_receive = BitmapFactory.decodeByteArray(raw_data, 0, raw_data.length);
                     if(isConnected == false)
                         break;
                     publishProgress();
                 }
-                outStream.write("Exit".getBytes("UTF-8"));
+                outJson.put("status", "exit");
+                writeUTF8(outJson.toString(), out);
+                //outStream.write("Exit".getBytes("UTF-8"));
             }
             catch (Exception e){
                 e.printStackTrace();
--- client/app/src/main/java/cctv/cse/com/cctv_client/SplashActivity.java
+++ client/app/src/main/java/cctv/cse/com/cctv_client/SplashActivity.java
@@ -26,5 +26,6 @@ public class SplashActivity extends AppCompatActivity {
             }
         };
         myThread.start();
+
     }
 }
