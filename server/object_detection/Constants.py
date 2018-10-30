9a90df623c422f92e6b6f049ae9e9357d0735b5b
KimSangYeon-DGU <sy0814k@gmail.com>
2018-10-30 20:47:44 +0900

    Edited) Constants.py

--- server/object_detection/Constants.py
+++ server/object_detection/Constants.py
@@ -8,9 +8,9 @@ ANDR_PORT = 8000
 CUR_DIR = os.getcwd()
 REC_DIR = os.path.join(CUR_DIR, "record")
 
-DB_ADDR = "https://database-9579c.firebaseio.com/"
-PUSH_ADDR = "AAAALAmx7fk:APA91bF5nUol6YBNmKvudG4YTC_AgOcZ0fxaoeJDWsVdDWXRd2JqDUR_9My3KzE_xqBKoWUHkKO4_f3j2hTFmvqj3glraPZHoR-3-6xMxzHaZt-bQL2KFE3_ds6ATAcBUggw18e7fbWM"
-REG_ID = "cE-v-DzRduI:APA91bG3d7KM0BKJV4OwLPm2fJnRcsYHqPXefAoyPgQwbL-e0TEaZOgQImQw25vZONk9k4YPkZFeE4UnvJdPvfivzTFZP-ZJfikx6FKRffCGOySVFQx4SRccT0WaUANHDH8oQOjUr2b4"
+DB_ADDR = "Database server address"
+PUSH_ADDR = "Push notification server address"
+REG_ID = "Registration ID"
 NOTIF_COUNT = 4
 QUEUE_SIZE = 30
 REC_FILE_NUM = 60
